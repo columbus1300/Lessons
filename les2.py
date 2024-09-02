34cb7df8c3c20688abeecefe9eda4d75e3057b10
columbus1300 <nick13.00@mail.ru>
2024-09-02 20:32:20 +0300

    Update les2.py

--- les2.py
+++ les2.py
@@ -1,5 +1,5 @@
-number_of_completed_HW = '12'
-number_of_hours = '1.5'
+number_of_completed_HW = 12
+number_of_hours = 1.5
 course_name = 'Python'
-time_for_one_task = float(number_of_hours)/float(number_of_completed_HW)
-print('Курс:'+course_name+', всего задач:'+number_of_hours+', затрачено часов:'+number_of_hours+', среднее время выполнения:'+str(time_for_one_task))
+time_for_one_task = number_of_hours/number_of_completed_HW
+print('Курс:'+course_name+', всего задач:'+str(number_of_hours)+', затрачено часов:'+str(number_of_hours)+', среднее время выполнения:'+str(time_for_one_task))
