a6a158f250f002a8d2a41ab62bbc58273d68cf72
columbus1300 <nick13.00@mail.ru>
2024-09-02 20:10:24 +0300

    Add files via upload

---
+++ les2.py
@@ -0,0 +1,5 @@
+number_of_completed_HW = '12'
+number_of_hours = '1.5'
+course_name = 'Python'
+time_for_one_task = float(number_of_hours)/float(number_of_completed_HW)
+print('Курс:'+course_name+', всего задач:'+number_of_hours+', затрачено часов:'+number_of_hours+', среднее время выполнения:'+str(time_for_one_task))
