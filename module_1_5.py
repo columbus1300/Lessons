d0fa6fbd1180d76a31b41940a78c44317a253ca9
columbus1300 <nick13.00@mail.ru>
2024-09-07 20:39:32 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 'two', 3.0
+print(immutable_var)
+#immutable_var[0]=2
+#print(immutable_var)
+#Кортежи (tuple) — неизменяемый тип данных
+mutable_list = [1,2,3, True]
+print(mutable_list)
+mutable_list[0]=2
+print(mutable_list)
